f40310df5f82efba27425833af506a7f29ee1778
Guilherme Vezula <guilherme.vezula@meiuca.design>
2025-10-04 13:03:47 -0300

    add back button into pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,37 @@
+import classNames from "classnames";
 import type React from "react";
+import ButtonIcon from "../ButtonIcon/ButtonIcon";
 import "./Header.scss";
 
-type HeaderProps = React.HTMLAttributes<HTMLHeadElement>;
+type HeaderProps = React.HTMLAttributes<HTMLHeadElement> & {
+  backButton?: boolean;
+  onBackClick?: () => void;
+};
 
 const CLASS_NAME = "header";
 
-const Header: React.FC<HeaderProps> = ({ children, ...props }) => {
-    return (
-        <header className={CLASS_NAME} {...props}>
-            <h1 className={`${CLASS_NAME}__title`}>{children}</h1>
-            <div className={`${CLASS_NAME}__line`} />
-        </header>
-    )
-}
+const Header: React.FC<HeaderProps> = ({
+  children,
+  backButton,
+  onBackClick,
+  ...props
+}) => {
+  return (
+    <header className={CLASS_NAME} {...props}>
+      <div
+        className={classNames({
+          [`${CLASS_NAME}__header`]: true,
+          [`${CLASS_NAME}__header--button`]: backButton,
+        })}
+      >
+        {backButton && (
+          <ButtonIcon border icon="arrow-prev" onClick={onBackClick} />
+        )}
+        <h1 className={`${CLASS_NAME}__title`}>{children}</h1>
+      </div>
+      <div className={`${CLASS_NAME}__line`} />
+    </header>
+  );
+};
 
 export default Header;
--- src/pages/MealPlan/MealPlan.tsx
+++ src/pages/MealPlan/MealPlan.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../app/hooks";
 import Button from "../../components/Button/Button";
 import CalendarGrid from "../../components/CalendarGrid/CalendarGrid";
@@ -9,13 +10,21 @@ const CLASS_NAME = "meal-plan";
 
 function MealPlan() {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const handleClearCalendar = () => {
     dispatch(removeAllMealPlan());
-  }
+  };
+
+  const handleBackNavigation = () => {
+    navigate("/recipes");
+  };
+
   return (
     <div className={CLASS_NAME}>
-      <Header>Weekly Meal Planner</Header>
+      <Header backButton onBackClick={handleBackNavigation}>
+        Weekly Meal Planner
+      </Header>
       <div className={`${CLASS_NAME}__actions`}>
         <Button onClick={handleClearCalendar}>Clear Calendar</Button>
         <Button>Generate Shopping List</Button>
