26736d36a8bb693953d833b09fcc8720c960f610
Guilherme Vezula <guilherme.vezula@meiuca.design>
2025-10-04 10:35:22 -0300

    add icons assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+import Add from "./add.svg";
+import Close from "./close.svg";
+import Delete from "./delete.svg";
+
+const IconSet = {
+    add: Add,
+    delete: Delete,
+    close: Close,
+};
+
+export default IconSet;
